50d20010bd84ca1ace8a1e143ad40a221e40e70b
Anıl Çelik <celikforbusiness@gmail.com>
2024-01-26 16:17:03 +0300

    Update kali-enablerdp-startup.sh

--- kali-enablerdp-startup.sh
+++ kali-enablerdp-startup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 command sudo apt-get update
-command sudo apt install kali-desktop-xfce
-command sudo apt install xrdp
+command sudo apt install kali-desktop-xfce -y
+command sudo apt install xrdp -y
 command sudo service xrdp start
 command sudo systemctl enable xrdp
 command sudo ufw allow 3389
